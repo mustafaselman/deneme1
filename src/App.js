5239774103363f361f2a20968f5140d934faa97f
mustafaselmanonal <mustafaselman.100@gmail.com>
2022-11-29 19:49:45 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {Provider} from "react-redux";
+import {store} from "./redux";
+import Router from "./config/router";
+import { CssBaseline } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+return (
+  <Provider store={store}>
+      <CssBaseline/>
+      <Router/>
+  </Provider>
+
+)
 }
-
-export default App;
